e6a67fad12317db0a6417ee93a1cd6fead471279
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-06-05 22:42:19 -0400

    finished phase-1-javascript-variables-lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// Declare and define companyName as a const
+const companyName = "Scuber";
+
+// Declare and define mostProfitableNeighborhood using let
+let mostProfitableNeighborhood = "Chelsea";
+
+// Declare and define companyCeo using let
+let companyCeo = "Susan Smith";
